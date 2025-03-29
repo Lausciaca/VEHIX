fb2563f983ff11c480abcb63022ce007c0308fbe
Lau <laureanosciacaluga@gmail.com>
2025-03-29 09:43:54 -0300

    Template para elegir cobertura de la orden

--- orden/urls.py
+++ orden/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from .views import *
 ordenpatterns = ([
-    path('', ordenes, name='list'),
-    path('create', crear_orden, name='create'),
-    path('orden/<slug:codigo>', ver_orden, name='detail'),
-    path('orden/<slug:codigo>/eliminar', eliminar_orden, name='delete'),
-    path('orden/cambiar_estado/<slug:codigo>/<str:estado>/', cambiar_estado, name='cambiar_estado'),
+    path('/', ordenes, name='list'),
+    path('/create', crear_orden, name='create'),
+    path('/orden/<slug:codigo>', ver_orden, name='detail'),
+    path('/orden/<slug:codigo>/eliminar', eliminar_orden, name='delete'),
+    path('/orden/cambiar_estado/<slug:codigo>/<str:estado>/', cambiar_estado, name='cambiar_estado'),
 ], 'orden')
