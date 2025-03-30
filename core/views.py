9a8ceb93c01adcd1a16178f72d9dbd483d96aaad
Laureano <laureanosciacaluga@gmail.com>
2025-03-30 16:33:13 -0300

    30-3

--- core/views.py
+++ core/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from orden.views import obtener_todas_las_ordenes
-# from orden.models import Orden
+from collections import Counter
+from orden.models import *
 
 # Create your views here.
 def index(request):
@@ -16,12 +17,42 @@ def index(request):
 
     # Contar las órdenes cuyo estado no es 'Entregado'
     activas = sum(1 for orden in ordenes if orden.get_estado_display() != 'Entregado')
-    # ordenes = Orden.objects.all().order_by('-created')[:3]
-    # ordenesActivas = Orden.objects.exclude(estado='entregado').count()
-    # vehiculosTaller = Orden.objects.filter(estado='taller').count()
+   
+    
+    cobertura_mas_elegida = obtener_cobertura_mas_repetida()
 
     return render(request, 'core/index.html', { 
         'ordenes': ultimas_3_ordenes, 
         'ordenesActivas' : activas,
-        'vehiculosTaller' : en_taller
+        'vehiculosTaller' : en_taller,
+        'coberturaMasElegida' : cobertura_mas_elegida
     })
+    
+    
+    
+def obtener_cobertura_mas_repetida():
+    # Recuperar todas las órdenes
+    ordenes_particulares = OrdenParticular.objects.all()
+    ordenes_terceros = OrdenTerceros.objects.all()
+    ordenes_riesgo = OrdenRiesgo.objects.all()
+    ordenes_recupero = OrdenRecupero.objects.all()
+
+    # Asignar las coberturas a cada tipo de orden
+    coberturas = (
+        ['Particular'] * len(ordenes_particulares) +
+        ['Contra terceros'] * len(ordenes_terceros) +
+        ['Todo riesgo'] * len(ordenes_riesgo) +
+        ['Recupero de siniestro'] * len(ordenes_recupero)
+    )
+
+    # Contar las frecuencias de cada cobertura
+    contador_coberturas = Counter(coberturas)
+
+    # Obtener la cobertura más repetida
+    cobertura_mas_repetida = contador_coberturas.most_common(1)
+
+    # Si existe alguna cobertura, devolver solo el texto de la cobertura más repetida
+    if cobertura_mas_repetida:
+        return cobertura_mas_repetida[0][0]  # Retorna solo el nombre de la cobertura más repetida
+    else:
+        return None
